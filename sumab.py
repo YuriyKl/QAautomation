502ff1ee4d5093ae799780bb95f5bbd8eaf6540e
YuriyKl <56383800+YuriyKl@users.noreply.github.com>
2023-03-31 23:10:02 +0300

    Add files via upload
    added multiplication to sumab.py file

--- sumab.py
+++ sumab.py
@@ -4,5 +4,6 @@ a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 
 print("The sum of element {0} and element {1} is: {2}".format("a", "b", a+b))
+print("Multiplication of elements {0} and {1} results to: {2}".format("a", "b", a*b))
 
 
