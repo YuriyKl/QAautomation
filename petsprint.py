433b87e05016f06c27796c9aa79fac31bdc27193
YuriyKl <56383800+YuriyKl@users.noreply.github.com>
2023-04-02 11:20:45 +0300

    Add files via upload

--- petsprint.py
+++ petsprint.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-
-
-print('Murzik', 'Barsik', 'Pantera', sep=', ')
+# Solution 1
+pets_aliases = ('Murzik', 'Barsik', 'Pantera')
+print(pets_aliases[0], end=", ")
+print(pets_aliases[1], end=", ")
+print(pets_aliases[2])
+# Solution 2
+for alias in pets_aliases:
+    if alias != pets_aliases[-1]:
+        print(alias, end=", ")
+    else:
+        print(alias)
