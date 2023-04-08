99b0c585b4b667af4ea285bf68a3f6d55dc58140
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-04-08 22:57:38 +0300

    script for palindrome check

--- HW_L4/checkpalindrom.py
+++ HW_L4/checkpalindrom.py
@@ -4,18 +4,11 @@
 check_palindrom = str(input("Please, enter any string for the palindrome check:"))
 
 # Solution 1
-lowstr = check_palindrom.casefold().replace(" ","")
+lowstr = check_palindrom.casefold().replace(" ","").strip()
 print("This string is palindrome" if lowstr == lowstr[::-1] else "This string is NOT palindrome")
 
 # Solution 2
-chkstr = check_palindrom.lower().replace(" ","")
+chkstr = check_palindrom.lower().replace(" ","").strip()
 revstr = reversed(chkstr)
 print("This string is palindrome" if list(chkstr) == list(revstr) else "This string is NOT palindrome")
 # print("This string is palindrome" if list(chkstr) == list(revstr) else "This string is NOT palindrome")
-
-#Solution 3 does not operates
-# upstr = check_palindrom.lower().replace(" ","").strip()
-# palindrom =[w for w in upstr if w == reversed(upstr)]
-# print(palindrom)
-# # palindrom =[w for w in upstr if w == reversed(upstr)]
-# print("This string is palindrome" if palindrom else "This string is NOT palindrome")
---
+++ HW_L4/lettercounter.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+
+check_palindrom = str(input("Please, enter any string for the palindrome check:"))
