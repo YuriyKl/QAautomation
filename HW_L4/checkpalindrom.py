737dc115b2ef7e7e10d57f15e22aeba06c0a7431
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-04-09 11:59:24 +0300

    script for palindrome check

--- HW_L4/checkpalindrom.py
+++ HW_L4/checkpalindrom.py
@@ -4,11 +4,11 @@
 check_palindrom = str(input("Please, enter any string for the palindrome check:"))
 
 # Solution 1
-lowstr = check_palindrom.casefold().replace(" ","").strip()
+lowstr = check_palindrom.casefold().strip()
 print("This string is palindrome" if lowstr == lowstr[::-1] else "This string is NOT palindrome")
 
 # Solution 2
-chkstr = check_palindrom.lower().replace(" ","").strip()
+chkstr = check_palindrom.lower().strip()
 revstr = reversed(chkstr)
 print("This string is palindrome" if list(chkstr) == list(revstr) else "This string is NOT palindrome")
 # print("This string is palindrome" if list(chkstr) == list(revstr) else "This string is NOT palindrome")
