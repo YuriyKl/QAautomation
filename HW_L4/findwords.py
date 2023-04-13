10b4e3700b231d19fc2d50b99c790f5cde2dcb66
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-04-13 15:58:00 +0300

    updated findwords.py with "join" concatenation string

--- HW_L4/findwords.py
+++ HW_L4/findwords.py
@@ -3,6 +3,6 @@
 sstring = "aab qq c badcc a qqqqqaqqqqaa tpara"
 
 newstr = sstring.split()
-print(newstr)
-s=[j.title() for j in newstr if j.count("a")==2]
-print(s)
+
+s = [j.title() for j in newstr if j.count("a") == 2]
+print(" ".join(str(e) for e in s))
