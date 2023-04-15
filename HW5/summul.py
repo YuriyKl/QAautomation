20e226d8439a22779ac5305b9f69ce5ecafb8eeb
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-04-15 21:50:07 +0300

    added summul.py script

--- HW5/summul.py
+++ HW5/summul.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+import math
 
 MIN = 3
 MAX = 6
 
 lst = [2, 4, 6, 2, 1, 1, 9, 4, 6]
-
-sumlst =sum ([i for i in lst if i>=MIN or i<=MAX])
-
+sumlst =sum(i for i in lst if i>=MIN if i<=MAX)
+multlst = math.prod(i for i in lst if i>=MIN if i<=MAX)
 print(sumlst)
+print(multlst)
 
 
--- HW_L4/findwords.py
+++ HW_L4/findwords.py
@@ -6,3 +6,7 @@ newstr = sstring.split()
 
 s = [j.title() for j in newstr if j.count("a") == 2]
 print(" ".join(str(e) for e in s))
+
+# better code will be
+# s = [j for j in sstring.split() if j.count("a") == 2]
+# print(" ".join(s).title())
