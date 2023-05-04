46689348b45512b55d9d5428f8c82c8f4e716d6e
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-05-04 10:15:29 +0300

    updated randb2str.py script

--- HW_L10/randb2str.py
+++ HW_L10/randb2str.py
@@ -3,9 +3,14 @@
 import random
 def get_random_string(length: int) -> str:
     lowalphabet = [chr(value) for value in range(97, 123)]
-    upperalphabet = [chr(value) for value in range(65, 90)]
-    nums = [chr(value) for value in range(48, 57)]
+    print(lowalphabet)
+    upperalphabet = [chr(value) for value in range(65, 91)]
+    print(upperalphabet)
+    nums = [chr(value) for value in range(48, 58)]
+    print(nums)
     reslst = [*lowalphabet, *upperalphabet, *nums]
-    print("".join(random.sample(reslst, k=length)))
+    # print("".join(random.sample(reslst, k=length)))
+    print("".join(random.choices(reslst, k=length)))
 
-get_random_string(9)
+get_random_string(180)
+
