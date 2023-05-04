43945a552d73931a2110c4c1e1b1f1ac69b19ed2
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-05-04 10:27:31 +0300

    updated randb2str.py script

--- HW_L10/randb2str.py
+++ HW_L10/randb2str.py
@@ -3,14 +3,12 @@
 import random
 def get_random_string(length: int) -> str:
     lowalphabet = [chr(value) for value in range(97, 123)]
-    print(lowalphabet)
     upperalphabet = [chr(value) for value in range(65, 91)]
-    print(upperalphabet)
     nums = [chr(value) for value in range(48, 58)]
-    print(nums)
     reslst = [*lowalphabet, *upperalphabet, *nums]
-    # print("".join(random.sample(reslst, k=length)))
-    print("".join(random.choices(reslst, k=length)))
+    # print("".join(random.sample(reslst, k=length))) '''this solution with 'random.sample' takes unique symbol
+    # from the string thus it causes limits string length to 62 (26+26+10) symbols'''
+    return print("".join(random.choices(reslst, k=length)))
 
 get_random_string(180)
 
