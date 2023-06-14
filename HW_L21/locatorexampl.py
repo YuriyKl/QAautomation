42cd92f035393c6ebbcce63d1a756455a246910f
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2023-06-14 15:47:36 +0300

    updated locatorexampl.py script

--- HW_L21/locatorexampl.py
+++ HW_L21/locatorexampl.py
@@ -21,10 +21,19 @@ driver.find_element(By.XPATH, "//*[@name = 'q']").send_keys("cheese" + Keys.RETU
 # first_result = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'h3')))
 first_result = wait.until(ec.presence_of_element_located((By.XPATH, './/h3')))
 first_result.click()
+
 # word = driver.find_element(By.LINK_TEXT, "casein")
 # word = driver.find_element(By.CSS_SELECTOR, "[title*=Casein]")
-word = driver.find_element(By.XPATH, ".//*[@title='Casein']")
+casein = driver.find_element(By.XPATH, "(.//*[@title='Casein'])[1]")
 
-print(word.text)
+# parmesan = driver.find_element(By.LINK_TEXT, "Parmesan")
+parmesan = driver.find_element(By.XPATH, ".//*[@title='Parmesan']")
 
+brie = driver.find_element(By.LINK_TEXT, "Brie")
+# brie = driver.find_element(By.XPATH, ".//*[@title='Brie']")
+
+print(parmesan.text)
+print(brie.text)
+brie.click()
+time.sleep(1)
 driver.close()
