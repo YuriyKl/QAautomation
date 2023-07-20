32d271c2102e98d46d783e6c6ca48175c953174d
YuriyKl <56383800+YuriyKl@users.noreply.github.com>
2023-07-20 19:36:27 +0300

    Add files via upload

--- HW_L8/seqgen.py
+++ HW_L8/seqgen.py
@@ -1,50 +1,10 @@
 #!/usr/bin/python3
-def generator(n, m):
-    # sq_gen = (i+1 for i in range(n))
-    def sq_gen():
-        for i in range(n+1):
-            i += 1
-            yield i
+# solution 1
+def generator(pows, nums):
+    for _pow in range(1,pows+1):
+        for num in range(1,nums+1):
+            yield pow(num,_pow)
 
-    # pow_gen = (pow(i+1, x+1) for x in range(m-1) for i in range(n+1))
-    def pow_gen():
-        for i in range(n+1):
-            for x in range(m-1):
-                yield pow(i+1,x+1)
-            yield n
-
-
-    q1 = sq_gen
-    q2 = pow_gen
-
-    # def combine():
-    #     sqgen = sq_gen()
-    #     powgen = pow_gen()
-    #     for i in sqgen:
-    #         yield i
-    #     for i in powgen:
-    #         yield i
-    #
-    # for i in combine():
-    #     print(i)
-    '''   function combine does not operate 
-    sqgen = sq_gen() 
-    TypeError: 'generator' object is not callable'''
-
-    # def combine():
-    #     q1 = sq_gen()
-    #     q2 = pow_gen()
-    #     yield from q1
-    #     yield from q2
-    #
-    # for i in combine():
-    #     print(i) # same issue q1 = sq_gen()
-    # TypeError: 'generator' object is not callable
-
-    import itertools
-    for i in itertools.chain(sq_gen(), pow_gen()):
-        print(i)
-
-
-
-generator(3,4)
+# solution 2
+def generator2(pows, nums):
+    return (pow(num,_pow) for _pow in range(1,pows+1) for num in range(1,nums+1))
